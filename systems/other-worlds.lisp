e3d0c74f72b448169b58d26c275e34e0f654ef76
Mart van de Wege <mvdwege@gmail.com>
2013-10-27 23:05:09 +0100

    add world-type support
    also assorted small fixes.

--- systems/other-worlds.lisp
+++ systems/other-worlds.lisp
@@ -1,7 +1,24 @@
 (in-package :traveller)
 ;;;; Generate other world types beyond Mainworlds.
 
+;;; Normal word types
 (defclass planetoids (world)
   ((size :initform (make-instance 'size :code 0))
    (atmosphere :initform (make-instance 'atmosphere :code 0))
    (hydrographics :initform (make-instance 'hydrographics :code 0))))
+
+(defclass iceworld (world) () )
+
+(defmethod slot-unbound (class (w iceworld) (slot (eql 'population)))
+  (setf (slot-value w 'population)
+	(make-instance 'population :code (max 0 (roll 2 :dm -6)))))
+
+;;; Gas Giants
+(defclass gas-giant  ()
+    ((size)))
+
+(defclass large-gas-giant (gas-giant) () )
+(defclass small-gas-giant (gas-giant) () )
+(defclass ice-giant (gas-giant) () )
+(defclass brown-dwarf (gas-giant secondary-star) 
+  ((size :initform (make-instance 'size :code 24))) )
