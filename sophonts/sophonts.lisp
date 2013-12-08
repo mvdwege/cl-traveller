5b16ec8a8e7591743c27398a137a19dfadba16a2
Mart van de Wege <mvdwege@gmail.com>
2013-12-08 21:27:54 +0100

    gender generation and minor fixes

--- sophonts/sophonts.lisp
+++ sophonts/sophonts.lisp
@@ -23,8 +23,14 @@
 	       :reader locomotion)
    (ecological-niche :initarg :ecological-niche
 		     :reader ecological-niche)
-   (caste :reader caste)
-   (genders :reader genders)))
+   (caste-table :initarg caste-table
+		:reader caste-table)
+   (gender-structure :initarg :gender-structure
+		     :reader gender-structure)
+   (gender-table :initarg :gender-table
+		 :reader gender-table)
+   (gender-differences :initarg :gender-differences
+		       :reader gender-differences)))
 
 ;; Keep the compiler happy. Especially SBCL insists on this method
 ;; existing. We're not doing really deep MOP things, so a simple "Yes,
@@ -34,15 +40,22 @@
 
 ;;; Homeworld generation
 (defmethod slot-unbound (class (sophont sophont-class) (slot (eql 'homeworld)))
-  (setf (slot-value sophont slot) (make-world)))
-
+  (setf (slot-value sophont slot)
+	;; Brute-force approach: generate worlds until we have one
+	;; suitable for life.
+	(do ((world (make-world) (make-world)))
+	    ((and
+	      (>= (population world) 7)
+	(>= (atmosphere world) 2)) world)
+	  ())))
+     
 ;;; Determine Native Terrain, Locomotion, and Ecological Niche.
 (defvar *terrain-types* 
   '(mountain desert exotic rough-wood rough clear forest wetlands
   wetland-woods ocean ocean-depths))
 
 (defvar *locomotion-types*
-  '((walker walker amphib amphib amphib walker walker amphib amphib flyphib Aquatic)
+  '((walker walker amphib amphib amphib walker walker amphib amphib flyphib aquatic)
     (walker walker walker walker walker walker walker aquatic walker swim diver)
     (walker walker walker walker walker walker walker walker walker swim diver)
     (walker walker walker walker walker walker walker walker walker swim diver)
@@ -89,19 +102,16 @@
   (setf (slot-value sophont slot) 
 	(min 5 (max -5 (+ (flux) (terrain-modifiers sophont))))))
 
-
 (defmethod native-terrain :around ((sophont-class sophont-class)) 
   (string-capitalize
    (symbol-to-name (nth (+ (call-next-method) 5) *terrain-types*))))
 
 (defmethod slot-unbound (class (sophont sophont-class) (slot (eql 'locomotion)))
   (setf (slot-value sophont slot) 
-	(flux-on 
-	 (nth (random 6) *locomotion-types*)
-	 :dm (native-terrain-mod sophont))))
+	(nth (+ 5 (native-terrain-mod sophont))(nth (random 6) *locomotion-types*))))
 
 (defmethod slot-unbound (class (sophont sophont-class) (slot (eql 'ecological-niche)))
-  (let ((basic-niche (nth (+ (flux) 6) *basic-niche*)))
+  (let ((basic-niche (flux-on *basic-niche* :shift 6)))
     (setf (slot-value sophont slot) 
 	  (list basic-niche 
 		(flux-on  
@@ -195,42 +205,84 @@
 	(push result result-list)))
     (setf (slot-value sophont slot) (nreverse result-list))))
 
+;;; Gender generation
+(defvar *gender-structure-table*
+  '(solitaire solitaire eab eab dual dual dual fmn fmn group group))
+
+;; Mapping from numbered genders to names
+(defvar *gender-structures*
+  '(solitaire (solo)
+    dual (female male)
+    fmn (female male neuter)
+    eab (egg-donor activator bearer)
+    group (one two three four five six)))
+
+(defvar *gender-tables*
+  `(solitaire (,@(loop repeat 11 collect 'solo))
+    dual (female female female female male female male male male female male)
+    eab (egg-donor egg-donor egg-donor activator egg-donor activator bearer 
+	bearer bearer activator bearer)
+    fmn (female female female male female male neuter neuter neuter male neuter)
+    group (six six four four two one three five five six six)))
+
+(defmethod slot-unbound (class (sophont sophont-class) (slot (eql 'gender-structure)))
+  (setf (slot-value sophont slot) (flux-on *gender-structure-table*)))
+
+(defmethod slot-unbound (class (sophont sophont-class) (slot (eql 'gender-table)))
+  ;; Initial gender assignments
+  (with-slots (gender-structure) sophont
+    (setf (slot-value sophont slot)
+	  (let ((gender-table))
+	    (unless (eql gender-structure 'group)
+	      (mapcar 
+		#'(lambda (gender) (push gender gender-table)) 
+		(getf *gender-structures* gender-structure)))
+	    (do () 
+		((= (length gender-table) 11) (nreverse gender-table))
+	      (push (flux-on (getf *gender-tables* gender-structure)) gender-table))))))
+
+(defmethod slot-unbound (class (sophont sophont-class) (slot (eql 'gender-differences)))
+  (let ((difference-table nil))
+    (dolist (g (remove-duplicates (gender-table sophont)))
+      (let ((differences nil))
+      (dotimes (c 6)
+	(let ((flux-roll (flux)))
+	  (push 
+	   ;; Special treatment for Strength and rolls between -1 and +1.
+	   (cond
+	     ((and (= c 0) (>= flux-roll 3)) (roll flux-roll))
+	     ((and (= c 0) (= flux-roll 1) 1))
+	     ((and (>= flux-roll -1) (<= flux-roll 1)) 0)
+	     (t flux-roll))
+	   differences)))
+      (setf (getf difference-table g) (nreverse differences))))
+    (setf (slot-value sophont slot) difference-table)))
+	
+	
+
+;;; Caste generation
+(defvar *caste-structure-table*
+  '(body economic family military social skilled))
+
+(defvar *caste-tables* 
+  '(body (healer gender antibody sensor memory muscle muscle muscle voice special claw)
+    economic (innovator gender guard researcher artisan laborer craftsman clerk manager 
+	      special entrepreneur)
+    family (healer gender defender caregiver caregiver breadwinner breadwinner
+	    breadwinner uncle special leader)
+    military (medic gender aide scout specialist soldier technician warrior leader 
+	      special staff)
+    social (artist gender enforcer drone artist unit unit unit patron special entertainer)
+    special (deminimus useless advisor- instructor shaman expendable defective valuable 
+	     advisor+ sport)))
+
+(defvar *caste-tables-uniques*
+  '(body brain economic director family archon military general social ruler))
+
 ;; sophont constructor
 (defmacro defsophont (&rest initargs)
   `(make-instance 'sophont-class
 		  :direct-superclasses (list (find-class 'sophont))
 		  ,@initargs))
-
-;;; Individual sophonts
-
-;; Base class for sophont individuals
-(defclass sophont () 
-  ((name :accessor name
-	 :initarg :name)
-   (homeworld :accessor homeworld
-	      :initarg :homeworld)
-   (birthworld :reader birthworld)
-   (age :accessor age
-       :initarg :age)
-   (characteristics :accessor characteristics
-		    :initarg :characteristics)
-   (genetics :accessor genetics))
-  (:metaclass sophont-class))
-
-;;; Individual Characteristics
-(defmethod slot-unbound (class (specimen sophont) (slot (eql 'characteristics)))
-  (let ((characteristic-list) (genetic-list))
-    (dotimes (c 6)
-      (let ((characteristic-value 0)
-	    (dice (nth c (characteristic-dice (class-of specimen)))))
-	(if (>= dice 4)
-	    (progn
-	      (- dice 2)
-	      (setf characteristic-value 12)))
-	(push (roll 1) genetic-list)
-	(incf characteristic-value (+ (roll (decf dice)) (car genetic-list)))
-	(push characteristic-value characteristic-list)))
-    (setf (slot-value specimen 'genetics) (nreverse genetic-list))
-    (setf (slot-value specimen slot) (nreverse characteristic-list))))
 
 
