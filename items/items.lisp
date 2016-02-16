c07c3a2207fba96cebe4f87113c3a45b333fd21d
Mart van de Wege <mvdwege@gmail.com>
2016-02-16 11:56:55 +0100

    Change QREBS s to safety using shadowed symbol

--- items/items.lisp
+++ items/items.lisp
@@ -2,11 +2,11 @@
 (in-package :traveller)
 
 ;;; Item base classe, with slots for QREBS (pp. 190-196). Safety is
-;;; renamed safe to deconflict with the 'safety' compiler directive
+;;; a shadowed symbol.
 (defclass item () 
   ((quality :accessor quality :initform 5 :type integer)
    (reliability :accessor reliability :initform 0 :type integer)
    (ease-of-use :accessor ease-of-use :initform 0 :type integer)
    (burden :accessor burden :initform 0 :type integer)
-   (safe :accessor safe :initform 0 :type integer)))
+   (safety :accessor safe :initform 0 :type integer)))
 
