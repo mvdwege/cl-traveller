9477a4f5827b6ac24143ba40cd547b7ce8034c26
Mart van de Wege <mvdwege@gmail.com>
2017-02-03 12:32:55 +0100

    Remove all instances of uwp-attribute objects
    We have abandoned setting the UWP attributes as objects, instead
    making them simple values. Gas giants were not updated to the new
    syntax yet.

--- systems/other-worlds.lisp
+++ systems/other-worlds.lisp
@@ -3,22 +3,22 @@
 
 ;;; Normal word types
 (defclass planetoids (world)
-  ((size :initform (make-instance 'size :code 0))
-   (atmosphere :initform (make-instance 'atmosphere :code 0))
-   (hydrographics :initform (make-instance 'hydrographics :code 0))))
+  ((size :initform 0)
+   (atmosphere :initform 0)
+   (hydrographics :initform 0)))
 
 (defclass iceworld (world) () )
 
 (defmethod slot-unbound (class (w iceworld) (slot (eql 'population)))
   (setf (slot-value w 'population)
-	(make-instance 'population :code (max 0 (roll 2 :dm -6)))))
+        (max 0 (roll 2 :dm -6))))
 
 (defclass bigworld (world) () )
 
 (defmethod slot-unbound (class (bigworld bigworld) (slot (eql 'size)))
   (setf (slot-value bigworld 'size)
-	(make-instance 'size :code 
-		       (do ((size 0 (roll 2 :dm 7))) ((>= size 11) size) ()))))
+        (do ((size 0 (roll 2 :dm 7))) ((>= size 11) size) ())))
+
 ;;; Gas Giants
 (defclass gas-giant (body)
     ((size :reader size)
@@ -28,7 +28,7 @@
 (defclass small-gas-giant (gas-giant) () )
 (defclass ice-giant (gas-giant) () )
 (defclass brown-dwarf (gas-giant secondary-star) 
-  ((size :initform (make-instance 'size :code 24))
+  ((size :initform 24)
    (spectrum :initform '(BD nil nil))))
 
 ;; (defmethod size :around ((self gas-giant))
@@ -39,6 +39,6 @@
     (if (<= random-size 22)
 	(change-class self 'small-gas-giant)
 	(change-class self 'large-gas-giant))
-    (setf (slot-value self 'size) (make-instance 'size :code random-size))))
+    (setf (slot-value self 'size) random-size)))
 
 
