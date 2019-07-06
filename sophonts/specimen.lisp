f7617769c1f70a5bde68d97ed1ad14523b8abde6
Mart van de Wege <mvdwege@gmail.com>
2019-07-06 19:18:34 +0200

    Add reader functions for Characteristics

--- sophonts/specimen.lisp
+++ sophonts/specimen.lisp
@@ -49,11 +49,134 @@
     (setf (slot-value specimen 'genetics) (nreverse genetic-list))
     (setf (slot-value specimen slot) (nreverse characteristic-list))))
 
+;;; Utility functions
+(defun halve-if (value name &rest characteristics)
+  (if (some #'(lambda (x) (eql name x)) characteristics)
+      (round (/ value 2))
+      value))
+
+;;; Characteristic handling
 (defgeneric c (specimen n)
   (:documentation "Generic function to get Characteristic n from a Sophont"))
 
 (defmethod c ((specimen sophont) n)
   (nth (- n 1) (characteristics specimen)))
+
+(defgeneric strength (specimen) )
+
+(defgeneric dexterity (specimen)
+  (:documentation "Return C2. Full value if C2=Dex, half value if C2=Agi or C2=Gra"))
+
+(defgeneric agility (specimen)
+  (:documentation "Return C2. Full value if C2=Agi, half value if C2=Dex or C2=Gra"))
+
+(defgeneric grace (specimen)
+  (:documentation "Return C2. Full value if C2=Gra, half value if C2=Agi or C2=Dex"))
+
+(defgeneric endurance (specimen)
+  (:documentation "Return C3. Full value if C2=End, half value if C3=Vig or C3=Sta"))
+
+(defgeneric vigor (specimen)
+  (:documentation "Return C3. Full value if C3=Vig, half value if C3=End or C3=Sta"))
+
+(defgeneric stamina (specimen)
+  (:documentation "Return C3. Full value if C3=Sta, half value if C3=Vig or C3=End"))
+
+(defgeneric education (specimen)
+  (:documentation "Return C5. Full value if C5=Edu, half value if C5=Tra or 4 if C5=Ins"))
+
+(defgeneric training (specimen)
+  (:documentation "Return C5. Full value if C5=Tra, half value if C5=Edu or 4 if C5=Ins"))
+
+(defgeneric instinct (specimen)
+  (:documentation "Return C5. Full value if C5=Ins, 4 if C5=Tra or C5=Edu"))
+
+(defgeneric social-standing (specimen)
+  (:documentation "Return C6. Full value if C6=Soc, full value if C6=Cha or 4 if C6=Cas"))
+
+(defgeneric charisma (specimen)
+  (:documentation "Return C6. Full value if C6=Cha, half value if C6=Soc or 4 if C6=Cas"))
+
+(defgeneric caste (specimen)
+  (:documentation "Return C6. Returns the Caste symbol if C6=Cas, 4 if called for Soc or Cha"))
+
+(defmethod strength ((specimen sophont))
+  (c specimen 1))
+
+;; For now we use the brute force method to check if we need to halve
+;; a Characteristic, defining mostly the same functions. Later we
+;; might refactor that, for example defining the methods
+;; programmatically.
+(defmethod dexterity ((specimen sophont))
+  (let ((value (c specimen 2))
+	(name (nth 1 (characteristics (class-of specimen)))))
+    (halve-if value name 'agility 'grace)))
+
+(defmethod agility ((specimen sophont))
+  (let ((value (c specimen 2))
+	(name (nth 1 (characteristics (class-of specimen)))))
+    (halve-if value name 'dexterity 'grace)))
+
+(defmethod grace ((specimen sophont))
+  (let ((value (c specimen 2))
+	(name (nth 1 (characteristics (class-of specimen)))))
+    (halve-if value name 'dexterity 'agility)))
+
+(defmethod endurance ((specimen sophont))
+  (let ((value (c specimen 3))
+	(name (nth 2 (characteristics (class-of specimen)))))
+    (halve-if value name 'stamina 'vigor)))
+
+(defmethod stamina ((specimen sophont))
+  (let ((value (c specimen 3))
+	(name (nth 2 (characteristics (class-of specimen)))))
+    (halve-if value name 'endurance 'vigor)))
+
+(defmethod vigor ((specimen sophont))
+  (let ((value (c specimen 3))
+	(name (nth 2 (characteristics (class-of specimen)))))
+    (halve-if value name 'endurance 'stamina)))
+
+(defmethod education ((specimen sophont))
+  (let ((value (c specimen 5))
+	(name (nth 4 (characteristics (class-of specimen)))))
+    (if (eq name 'instinct)
+	4
+	(halve-if value name 'training))))
+
+(defmethod training ((specimen sophont))
+  (let ((value (c specimen 5))
+	(name (nth 4 (characteristics (class-of specimen)))))
+    (if (eq name 'instinct)
+	4
+	(halve-if value name 'education))))
+
+(defmethod instinct ((specimen sophont))
+  (let ((value (c specimen 5))
+	(name (nth 4 (characteristics (class-of specimen)))))
+    (if (or (eq name 'training) (eq name 'education))
+	4
+	value)))
+
+(defmethod social-standing ((specimen sophont))
+  (let ((value (c specimen 6))
+	(name (nth 5 (characteristics (class-of specimen)))))
+    (if (eq name 'caste)
+	4
+	value)))
+
+(defmethod charisma ((specimen sophont))
+  (let ((value (c specimen 6))
+	(name (nth 5 (characteristics (class-of specimen)))))
+    (cond ((eq name 'social-standing) (/ value 2))
+	  ((eq name 'caste) 4)
+	  (t value))))
+
+(defmethod caste :around ((specimen sophont))
+  (let ((name (nth 5 (characteristics (class-of specimen)))))
+    (if (or (eq name 'social-standing) (eq name 'charisma))
+	4
+	(call-next-method))))
 
 (defmethod slot-unbound (class (specimen sophont) (slot (eql 'genetics)))
   (let ((genetics nil))
