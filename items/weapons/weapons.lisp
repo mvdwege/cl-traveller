3f250210e8ecc87f20056561fdb425d218dc2d6d
Mart van de Wege <mart@vdwege.eu>
2019-08-14 15:54:27 +0200

    Refactor to use weapon-subtype class
    Use composition, use a weapon-subtype object to fill the
    weapon-subtype slot, instead of dynamically querying the data
    structures on every call.

--- items/weapons/weapons.lisp
+++ items/weapons/weapons.lisp
@@ -12,14 +12,26 @@
    (portability :initarg :portability :reader portability))
   (:documentation "Use this metaclass to create a weapon class (i.e. a model from a specific manufacturer), then instantiate that class to produce individual weapons"))
 
+(defclass weapon-subtype (item-descriptor)
+  ((category :initarg :category :reader category)
+   (range :initarg :range :reader range)
+   (mass :initarg :mass :reader mass)
+   (burden :initarg :burden :reader burden)
+   (hits-1 :initarg :h1 :reader h1)
+   (damage-dice-1 :initarg :d1 :reader d1)))
+
 (defmethod slot-unbound (class (weapon weapon-class) (slot (eql 'weapon-type)))
   (setf (slot-value weapon slot) (getf (roll-on *weapon-types-table*) :type)))
 
 (defmethod slot-unbound (class (weapon weapon-class) (slot (eql 'weapon-subtype)))
   (let* ((wt (weapon-type weapon))
-     (subtypes-table
-      (getf (find-if (lambda (x) (eql (getf x :type) wt)) *weapon-types-table*) :subtypes)))
-    (setf (slot-value weapon slot) (roll-on subtypes-table))))
+         (subtypes-table
+          (getf (find-if (lambda (x) (eql (getf x :type) wt)) *weapon-types-table*) :subtypes))
+         (random-subtype (roll-on subtypes-table))
+         (subtype-args (getf *weapon-subtypes-table* random-subtype)))
+    (setf (slot-value weapon 'weapon-subtype)
+          (apply #'make-instance
+                 (append (list 'weapon-subtype :descriptor random-subtype) subtype-args)))))
 
 (defmethod slot-unbound (class (weapon weapon-class) (slot (eql 'descriptor)))
   (let* ((wt (weapon-type weapon))
